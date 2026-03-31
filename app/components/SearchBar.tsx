8159b0fe8a0ab467e284230dae8e5df25ec41dcc
Javi <Jjaviergonzalez23@gmail.com>
2026-03-31 14:42:32 +0200

    fix: Form buscador responsive

--- app/components/SearchBar.tsx
+++ app/components/SearchBar.tsx
@@ -19,18 +19,18 @@ export const SearchBar = ({ initialSearch = '' }: SearchBarProps) => {
   return (
     <form
       onSubmit={handleSubmit}
-      className="flex justify-center w-full mx-20 py-6"
+      className="flex flex-col md:flex-row justify-center w-full md:mx-20 py-6"
     >
       <input
         type="text"
         value={searchTerm}
         placeholder="Buscar película por título"
         onChange={(e) => setSearchTerm(e.target.value)}
-        className="w-[80%] px-5 py-2 mr-8 bg-[#232F48] rounded-md focus:outline-none"
+        className="w-full md:w-[80%] px-5 py-2 mr-8 bg-[#232F48] rounded-md focus:outline-none"
       />
 
       <button
-        className="cursor-pointer py-3 px-8 rounded-md bg-[#232F48]"
+        className="cursor-pointer mt-2 md:mt-0 py-3 px-8 rounded-md bg-[#232F48]"
         type="submit"
       >
         Buscar
