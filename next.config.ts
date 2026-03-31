65f98fabd460dd31251535cd72a86e904ed4863a
Javi <Jjaviergonzalez23@gmail.com>
2026-04-01 00:00:59 +0200

    fix: Error corregido de redirección

--- next.config.ts
+++ next.config.ts
@@ -4,15 +4,6 @@ const nextConfig: NextConfig = {
   images: {
     domains: ['image.tmdb.org'],
   },
-  async redirects() {
-    return [
-      {
-        source: '/',
-        destination: '/search',
-        permanent: true,
-      },
-    ];
-  },
 };
 
 export default nextConfig;
